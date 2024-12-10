71459e955199be73966601b87a57eb2e8140e6db
nightskyyy <maranan.jkf@gmail.com>
2024-12-10 22:32:44 +0800

    star system, prefab resize

--- Assets/Script/Obstacle/Destroy.cs
+++ Assets/Script/Obstacle/Destroy.cs
@@ -6,25 +6,24 @@ public class Destroy : MonoBehaviour
 {
     public GameObject prefabToSpawn;  
     public GameObject spawnLocation;  
+    public string targetObjectNameInNextScene; 
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-           
             Destroy(gameObject);
-           // Debug.Log($"{gameObject.name} nasera");
 
-           
             if (prefabToSpawn != null && spawnLocation != null)
             {
-               
                 Instantiate(prefabToSpawn, spawnLocation.transform.position, spawnLocation.transform.rotation);
                 Debug.Log($"{prefabToSpawn.name} has been spawned at {spawnLocation.name}.");
             }
-            else
+
+            if (!string.IsNullOrEmpty(targetObjectNameInNextScene))
             {
-                //Debug.LogWarning("inde lomabas");
+                GameStateManager.Instance.MarkObjectVisible(targetObjectNameInNextScene);
+                Debug.Log($"{targetObjectNameInNextScene} will be visible in the next scene.");
             }
         }
     }
