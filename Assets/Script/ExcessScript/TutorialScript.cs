282fa4d91af7bf110c7f6c0c725ebd193fb92218
hamletbadua <135090605+hamletbadua@users.noreply.github.com>
2025-01-30 11:38:00 +0800

    Map and Tut
    Adjust Map Tracker
    Tutorial Script Fix

--- Assets/Script/ExcessScript/TutorialScript.cs
+++ Assets/Script/ExcessScript/TutorialScript.cs
@@ -6,10 +6,32 @@ public class TutorialScript : MonoBehaviour
 {
     public GameObject tutorialJump, tutorialGrab;
     public Animator tobyAnimator;
+    public bool isJump;
+    public bool isGrab;
     // Start is called before the first frame update
     void Start()
     {
         tobyAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isJump)
+            {
+                tobyAnimator.SetBool("idle", false);
+                tutorialJump.SetActive(false);
+                ClassicMapForce.instance.classicMapSpeed = 40f;
+                isJump = false;
+            }
+
+        if (Input.GetKeyDown(KeyCode.G) && isGrab)  // Press G to collect item
+            {
+                tobyAnimator.SetBool("idle", false);
+                tutorialGrab.SetActive(false);
+                ClassicMapForce.instance.classicMapSpeed = 40f;
+                isGrab = false;
+            }
+        
     }
 
     private void OnTriggerEnter(Collider col)
@@ -19,6 +41,7 @@ public class TutorialScript : MonoBehaviour
             tobyAnimator.SetBool("idle", true);
             tutorialJump.SetActive(true);
             ClassicMapForce.instance.classicMapSpeed = 0;
+            isJump = true;
         }
         
         if (col.gameObject.tag == "TutorialGrab")
@@ -26,29 +49,20 @@ public class TutorialScript : MonoBehaviour
             tobyAnimator.SetBool("idle", true);
             tutorialGrab.SetActive(true);
             ClassicMapForce.instance.classicMapSpeed = 0;
+            isGrab = true;
         }
     }
 
-    private void OnTriggerStay(Collider col)
-    {
-        if (col.gameObject.tag == "TutorialJump")
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                tobyAnimator.SetBool("idle", false);
-                tutorialJump.SetActive(false);
-                ClassicMapForce.instance.classicMapSpeed = 40f;
-            }
-        }
+    // private void OnTriggerStay(Collider col)
+    // {
+    //     if (col.gameObject.tag == "TutorialJump")
+    //     {
+            
+    //     }
 
-        if (col.gameObject.tag == "TutorialGrab")
-        {
-            if (Input.GetKeyDown(KeyCode.G))  // Press G to collect item
-            {
-                tobyAnimator.SetBool("idle", false);
-                tutorialGrab.SetActive(false);
-                ClassicMapForce.instance.classicMapSpeed = 40f;
-            }
-        }
-    }
+    //     if (col.gameObject.tag == "TutorialGrab")
+    //     {
+            
+    //     }
+    // }
 }
