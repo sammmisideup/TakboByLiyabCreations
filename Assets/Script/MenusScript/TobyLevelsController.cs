12a2b68e08497cb59afd632690494ec49366fd7e
Luck3rd <agento.edmon03@gmail.com>
2025-02-07 22:06:20 +0800

    TobyCutScene
    Change the cutscene to trigger in level 1
    DelayNext Button

--- Assets/Script/MenusScript/TobyLevelsController.cs
+++ Assets/Script/MenusScript/TobyLevelsController.cs
@@ -7,6 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class TobyLevelsController : MonoBehaviour
 {
+    public GameObject[] tobyCS;
+    public int tobyIndex;
+    public GameObject cutSceneToby, nextButton, playButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,45 @@ public class TobyLevelsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for(int i = 0; i < tobyCS.Length; i++)
+        if(i == tobyIndex)
+        {
+            tobyCS[i].SetActive(true);
+        }
+        else
+        {
+            tobyCS[i].SetActive(false);
+        }
         
+        if(tobyIndex == tobyCS.Length)
+        {
+            cutSceneToby.SetActive(false);
+        }
+
+        if(tobyIndex == 1)
+        {
+            nextButton.SetActive(true);
+        }
+        if(tobyIndex == 11)
+        {
+            nextButton.SetActive(false);
+            Invoke("DelayPlay", 8f);
+        }
+    }
+    
+    public void NextCutScene()
+    {
+        nextButton.SetActive(false);
+        tobyIndex++;
+        Invoke("DelayNext", 5f);
+    }
+    public void DelayNext()
+    {
+        nextButton.SetActive(true);
+    }
+    public void DelayPlay()
+    {
+        playButton.SetActive(true);
     }
 
     public void TobyLevel1()
@@ -43,4 +85,7 @@ public class TobyLevelsController : MonoBehaviour
     {
         SceneManager.LoadScene("Toby6");
     }
+
+
+    
 }
