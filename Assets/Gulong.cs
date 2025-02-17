505e31fcd27f8d33a8249817295c9feacc03a6f5
nightskyyy <maranan.jkf@gmail.com>
2025-02-17 19:10:24 +0800

    Tere level 1-5

---
+++ Assets/Gulong.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gulong : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 200f;
+
+    void Update()
+    {
+        
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+       
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
